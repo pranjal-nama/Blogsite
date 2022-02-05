82f19eecd99cf792f04d319d5897638f8724bbf8
Pranjal Nama <pranjalnama01@gmail.com>
2022-02-05 19:25:26 +0530

    updated home page

---
+++ index.js
@@ -0,0 +1,25 @@
+// let all_blogs = '';
+// let blog = '';
+
+// const post = {
+//     para :  myFunction(), 
+//     title : 'Imperfectly perfect',
+//     date : 4 Jan 2021,
+// }
+
+// function myFunction() {
+//     var x = document.createElement("P");
+//     var t = document.createTextNode("Hello everyone...
+
+//     I never knew that someday i'll be going to write a blog but yeah here i am. I always wanted to share my thoughts and experiences with everyone.
+    
+//     I'm a life lover and live every little moment of my life. I want this to be an awesome journey. I guess life teaches us at every step, sometimes we learn and sometimes we just go with the flow.
+//     Loving yourself is the most important part of your life . From the day i started loving myself, everything changed, everything seemed a little easy.
+//     One thing i learned about life is that life goes on, no matter what. You must live the life to its fullest. And never give yourself the chance for any regrets.
+//     And sometimes my mind feels like trapped. I'm a person lost in my own thoughts, trying to know me better and this world too.
+//      So let's begin this journey.....❤️");
+//     x.appendChild(t);
+//     document.body.appendChild(x);
+// }
+  
+console.log("Hello World")
