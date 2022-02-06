5482996838dc6af144c1e9a8374d9637df18e9de
Pranjal Nama <pranjalnama01@gmail.com>
2022-02-06 17:36:19 +0530

    added carousel

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 // let all_blogs = '';
 // let blog = '';
 
-// const post = {
-//     para :  myFunction(), 
-//     title : 'Imperfectly perfect',
-//     date : 4 Jan 2021,
-// }
+var slideIndex = 0;
+showSlides();
 
-// function myFunction() {
-//     var x = document.createElement("P");
-//     var t = document.createTextNode("Hello everyone...
-
-//     I never knew that someday i'll be going to write a blog but yeah here i am. I always wanted to share my thoughts and experiences with everyone.
-    
-//     I'm a life lover and live every little moment of my life. I want this to be an awesome journey. I guess life teaches us at every step, sometimes we learn and sometimes we just go with the flow.
-//     Loving yourself is the most important part of your life . From the day i started loving myself, everything changed, everything seemed a little easy.
-//     One thing i learned about life is that life goes on, no matter what. You must live the life to its fullest. And never give yourself the chance for any regrets.
-//     And sometimes my mind feels like trapped. I'm a person lost in my own thoughts, trying to know me better and this world too.
-//      So let's begin this journey.....❤️");
-//     x.appendChild(t);
-//     document.body.appendChild(x);
-// }
-  
-console.log("Hello World")
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
